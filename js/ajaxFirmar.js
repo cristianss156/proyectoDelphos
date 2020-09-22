f926f330387daa34774c3f4a563e6ba608051528
cristianss156 <cristian4silvestre@gmail.com>
2020-09-22 14:43:33 +0200

    Changes on JS and PHP

--- js/ajaxFirmar.js
+++ js/ajaxFirmar.js
@@ -1,124 +1,122 @@
-var objAjax;
+import { serverCall, rutaPhp, mensaje } from "./module/functions.js";
+
 var tipoBusqueda;
 var V_dni;
 
-function crearAjax(){
-	if(window.XMLHttpRequest){
-		objAjax=new XMLHttpRequest();
-	}
-	else{
-		objAjax = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-}
+/* funcion que recibe el dni del alumno y el tipo de busqueda que hay que hacer (amonestaciones o expulsiones)
+ y realiza la busqueda*/
+ var inputs = document.getElementsByClassName("filtro");
+ for (let i = 0; i < inputs.length; i++) {
+ 	inputs[i].addEventListener("click", function() {
+	 	clearTable();
 
-//funcion que recibe el dni del alumno y el tipo de busqueda que hay que hacer (amonestaciones o expulsiones)
-//y realiza la busqueda
-function buscar(_tipo, _dni){
-	crearAjax();
+		var datosBusqueda = {
+			tipo: this.value,
+			dni: document.getElementById("dni").value
+		};
 
-	if(document.getElementById("contenedorTabla").hasChildNodes()){
-		document.getElementById("contenedorTabla").removeChild(document.getElementById("table"));
-	}
+		tipoBusqueda = _tipo;
+		V_dni = document.getElementById("dni").value;
 
-	var datosBusqueda={
-		tipo:_tipo,
-		dni:_dni
+		var jsonstring = JSON.stringify(datosBusqueda);
+		let ruta = rutaPhp + "buscar.php?datosBusqueda=" + jsonstring;
+		serverCall(ruta, recibirBusqueda);
+	 })
+ };
+
+/*function buscar( _tipo, _dni ){
+	clearTable();
+
+	var datosBusqueda = {
+		tipo: _tipo,
+		dni: _dni
 	};
 
 	tipoBusqueda=_tipo;
 	V_dni=_dni;
 
-	var jsonstring=JSON.stringify(datosBusqueda);
-	objAjax.open("GET","http://localhost/html/proyectoDelphos/php/buscar.php?datosBusqueda="+jsonstring, true);
-	objAjax.send();
-	objAjax.onreadystatechange=recibirBusqueda;
-}
+	var jsonstring = JSON.stringify(datosBusqueda);
+	let ruta = rutaPhp + "buscar.php?datosBusqueda=" + jsonstring;
+	serverCall(ruta, recibirBusqueda);
+}*/
 
-//funcion que comprueba la respuesta del servidor y muestra un mensaje de informacion,
-//si la respuesta es correcta se llama a una funcion concreta dependiendo de si se quiere firma una amonestacion o una expulsion
-function recibirBusqueda(){
-	if(objAjax.readyState === 4 && objAjax.status === 200){
-		if(objAjax.responseText==="Sin resultado"){
-			if(document.getElementById("contenedorTabla").hasChildNodes()){
-				document.getElementById("contenedorTabla").removeChild(document.getElementById("table"));
-			}
-			document.getElementById("fech_firma").min="";
-			document.getElementById("fech_firma").value="";
-			document.getElementById("mensaje").innerHTML="";
-			document.getElementById("mensaje").innerHTML="No se han encontrado datos que coincidan con tu busqueda";
-			document.getElementById("mensaje_info").style.display="block";
-		}
-		else{
-			var datosBusqueda=[];
-			datosBusqueda=JSON.parse(objAjax.responseText);
-			if(tipoBusqueda==="amonestaciones"){
-				formatearBusqueda(datosBusqueda);
-			}
-			else if(tipoBusqueda==="expulsiones"){
-				formatearBusquedaExpulsiones(datosBusqueda);
-			}
+/* funcion que comprueba la respuesta del servidor y muestra un mensaje de informacion,
+si la respuesta es correcta se llama a una funcion concreta dependiendo de si se quiere 
+firma una amonestacion o una expulsion*/
+function recibirBusqueda( response ){
+	if(response === "Sin resultado") {
+		clearTable();
+
+		document.getElementById("fech_firma").min = "";
+		document.getElementById("fech_firma").value = "";
+		mensaje("No se han encontrado datos que coincidan con tu busqueda");
+	} else {
+		let datosBusqueda = [];
+		datosBusqueda = JSON.parse(response);
+		if(tipoBusqueda === "amonestaciones") {
+			formatearBusqueda(datosBusqueda);
+		} else if(tipoBusqueda === "expulsiones") {
+			formatearBusquedaExpulsiones(datosBusqueda);
 		}
 	}
 }
 
-//funcion que formatea los datos de las amonestaciones en una tabla
+// funcion que formatea los datos de las amonestaciones en una tabla
 function formatearBusqueda(ArrayBusqueda){
-	if(document.getElementById("contenedorTabla").hasChildNodes()){
-		document.getElementById("contenedorTabla").removeChild(document.getElementById("table"));
-	}
+	clearTable();
 
-	var tabla=document.createElement('table');
+	var tabla = document.createElement('table');
 	tabla.setAttribute("id", "table");
 
-	for(var i in ArrayBusqueda){
-		tr=document.createElement('tr');
+	for(var i in ArrayBusqueda) {
+		var tr = document.createElement('tr');
 
-	td=document.createElement('td');
-	td.innerHTML=V_dni;
-	td.setAttribute("class", "celda");
-	tr.appendChild(td);
+		td = document.createElement('td');
+		td.innerHTML = V_dni;
+		td.setAttribute("class", "celda");
+		tr.appendChild(td);
 
-	td=document.createElement('td');
-	td.innerHTML=ArrayBusqueda[i].Nombre;
-	td.setAttribute("class", "celda");
-	tr.appendChild(td);
+		td = document.createElement('td');
+		td.innerHTML = ArrayBusqueda[i].Nombre;
+		td.setAttribute("class", "celda");
+		tr.appendChild(td);
 
-	td=document.createElement('td');
-	td.innerHTML=ArrayBusqueda[i].NombreAsig;
-	td.setAttribute("class", "celda");
-	tr.appendChild(td);
+		td = document.createElement('td');
+		td.innerHTML = ArrayBusqueda[i].NombreAsig;
+		td.setAttribute("class", "celda");
+		tr.appendChild(td);
 
-	td=document.createElement('td');
-	td.innerHTML="Pepe";
-	td.setAttribute("class", "celda");
-	tr.appendChild(td);
+		td = document.createElement('td');
+		td.innerHTML = "Pepe";
+		td.setAttribute("class", "celda");
+		tr.appendChild(td);
 
-	td=document.createElement('td');
-	td.innerHTML=ArrayBusqueda[i].descripcion;
-	td.setAttribute("class", "celda");
-	tr.appendChild(td);
+		td = document.createElement('td');
+		td.innerHTML = ArrayBusqueda[i].descripcion;
+		td.setAttribute("class", "celda");
+		tr.appendChild(td);
 
-	td=document.createElement('td');
-	boton=document.createElement('input');
-	boton.setAttribute("type","radio");
-	boton.setAttribute("id",i);
-	boton.setAttribute("name","RBFirma");
-	boton.setAttribute("value",ArrayBusqueda[i].CodAmonestacion);
-	boton.setAttribute("onclick","aplicarFecha('"+ArrayBusqueda[i].Fecha_Amonestacion+"'), recogerCod(this.value)");
-	label=document.createElement("label");
-	label.setAttribute("for",i);
-	out=document.createElement("div");
-	out.setAttribute("class","outsiderb");
-	dentro=document.createElement("div");
-	dentro.setAttribute("class","insiderb");
-	out.appendChild(dentro);
-	label.appendChild(out);
-	td.appendChild(boton);
-	td.appendChild(label);
-	td.setAttribute("class", "boton");
-	tr.appendChild(td);
+		td=document.createElement('td');
+		boton=document.createElement('input');
+		boton.setAttribute("type","radio");
+		boton.setAttribute("id",i);
+		boton.setAttribute("name","RBFirma");
+		boton.setAttribute("value",ArrayBusqueda[i].CodAmonestacion);
+		boton.setAttribute("onclick","aplicarFecha('"+ArrayBusqueda[i].Fecha_Amonestacion+"'), recogerCod(this.value)");
+		label=document.createElement("label");
+		label.setAttribute("for",i);
+		out=document.createElement("div");
+		out.setAttribute("class","outsiderb");
+		dentro=document.createElement("div");
+		dentro.setAttribute("class","insiderb");
+		out.appendChild(dentro);
+		label.appendChild(out);
+		td.appendChild(boton);
+		td.appendChild(label);
+		td.setAttribute("class", "boton");
+		tr.appendChild(td);
 
-	tabla.appendChild(tr);
+		tabla.appendChild(tr);
 	}
 
 	document.getElementById("contenedorTabla").appendChild(tabla);
@@ -126,15 +124,13 @@ function formatearBusqueda(ArrayBusqueda){
 
 //funcion que formatea los datos de las expulsiones en una tabla
 function formatearBusquedaExpulsiones(ArrayBusqueda){
-	if(document.getElementById("contenedorTabla").hasChildNodes()){
-		document.getElementById("contenedorTabla").removeChild(document.getElementById("table"));
-	}
+	clearTable()
 
-	var tabla=document.createElement('table');
+	var tabla = document.createElement('table');
 	tabla.setAttribute("id", "table");
 
 	for(var i in ArrayBusqueda){
-		tr=document.createElement('tr');
+		var tr = document.createElement('tr');
 
 	td=document.createElement('td');
 	td.innerHTML=V_dni;
@@ -185,4 +181,10 @@ function formatearBusquedaExpulsiones(ArrayBusqueda){
 	}
 
 	document.getElementById("contenedorTabla").appendChild(tabla);
+}
+
+const clearTable = () => {
+	if(document.getElementById("contenedorTabla").hasChildNodes()) {
+		document.getElementById("contenedorTabla").removeChild(document.getElementById("table"));
+	}
 }
--- php/buscar.php
+++ php/buscar.php
@@ -1,27 +1,25 @@
 <?php
 
-$conexion=mysqli_connect("localhost", "root", "root", "delphosdbcristian") or die("Fallo en la conexion.");
+$conexion = mysqli_connect("localhost", "root", "root", "delphosdbcristian") or die("Fallo en la conexion.");
 
-$datosBusqueda=$_REQUEST["datosBusqueda"];
-$datos_php=json_decode($datosBusqueda, true);
+$datosBusqueda = $_REQUEST["datosBusqueda"];
+$datos_php = json_decode($datosBusqueda, true);
 
-if($datos_php["tipo"]==="amonestaciones"){
-	$result=mysqli_query($conexion, "select a.CodAmonestacion, a.Fecha_Amonestacion, alu.Nombre, asig.NombreAsig, c.descripcion from amonestaciones a, alumnos alu, asignaturas asig, causas_amonestacion c where idalumno='".$datos_php["dni"]."'  and dni='".$datos_php["dni"]."' and asig.CodAsignatura=a.CodAsignatura and CodCausa_Amonestacion=a.CausaAmonestacion and a.Fecha_Firma is null") or die ("Error al buscar.");
+if($datos_php["tipo"] === "amonestaciones") {
+	$result = mysqli_query($conexion, "select a.CodAmonestacion, a.Fecha_Amonestacion, alu.Nombre, asig.NombreAsig, c.descripcion from amonestaciones a, alumnos alu, asignaturas asig, causas_amonestacion c where idalumno='".$datos_php["dni"]."' and dni='".$datos_php["dni"]."' and asig.CodAsignatura=a.CodAsignatura and CodCausa_Amonestacion=a.CausaAmonestacion and a.Fecha_Firma is null") or die ("Error al buscar.");
+} else if($datos_php["tipo"] === "expulsiones") {
+	$result = mysqli_query($conexion, "select e.CodExpulsiones, e.Fecha_Expulsion, alu.Nombre, asig.NombreAsig, c.descripcion from expulsiones e, alumnos alu, asignaturas asig, causa_expulsion c where idalumno='".$datos_php["dni"]."'  and dni='".$datos_php["dni"]."' and asig.CodAsignatura=e.CodAsignatura and CodCausa_Expulsion=e.CausaExpulsion and e.Fecha_Firma is null") or die ("Error al buscar.");
 }
-else if($datos_php["tipo"]==="expulsiones"){
-	$result=mysqli_query($conexion, "select e.CodExpulsiones, e.Fecha_Expulsion, alu.Nombre, asig.NombreAsig, c.descripcion from expulsiones e, alumnos alu, asignaturas asig, causa_expulsion c where idalumno='".$datos_php["dni"]."'  and dni='".$datos_php["dni"]."' and asig.CodAsignatura=e.CodAsignatura and CodCausa_Expulsion=e.CausaExpulsion and e.Fecha_Firma is null") or die ("Error al buscar.");
-}
 
-$i=0;
+$i = 0;
 
-while($fila=mysqli_fetch_array($result)){
+while($fila = mysqli_fetch_array($result)) {
 	$resultBusqueda[$i]=$fila;
 	$i++;
 }
-if(isset($resultBusqueda)){
+if(isset($resultBusqueda)) {
 	echo json_encode($resultBusqueda);
-}
-else{
+} else{
 	echo "Sin resultado";
 }
 
